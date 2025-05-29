ef0dfb9640df1f05d9e1eaf711913cd3e182c140
ThuyTrang1109 <trangnttse184351@fpt.edu.vn>
2025-05-29 07:10:03 +0700

    Add files via upload

---
+++ Code/Chapter02/StringInteraction/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringInteraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string ho = "Nguyễn";
+            string tenlot = "Thị";
+            string ten = "Tèo";
+            string tenfull = ho + " " + tenlot + " " + ten;
+            Console.WriteLine(tenfull);
+            StringBuilder sb = new StringBuilder();  // cọng chỗi sẽ chậm, nên dùng string builder sẽ nhanh hơn
+            sb.Append(ho);
+            sb.Append(" ");
+            sb.Append(tenlot);
+            sb.Append(" ");
+            sb.Append(ten);
+        }
+    }
+}
